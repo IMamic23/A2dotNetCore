de83aa9627b732d9dd3f5144b14cda14bc2f1329
IMamic23 <mamic.ivica@gmail.com>
2017-08-29 00:07:27 +0200

    google maps demo

---
+++ ClientApp/app/components/google-maps/google-maps.component.ts
@@ -0,0 +1,239 @@
+import { FormControl } from '@angular/forms';
+import { ElementRef, NgZone, Component, OnInit, ViewChild } from '@angular/core';
+import { } from 'googlemaps';
+import { MapsAPILoader } from '@agm/core';
+
+@Component({
+  selector: 'rb-google-maps',
+  templateUrl: './google-maps.component.html',
+  styleUrls: ['./google-maps.component.css']
+})
+export class GoogleMapsComponent implements OnInit {
+
+  public latitude: number;
+  public longitude: number;
+  public searchControl: FormControl;
+  public zoom: number;
+  marker;
+  request;
+  map: google.maps.Map;
+  places: any[];
+  currentLocation: any;
+
+  @ViewChild("search")
+  public searchElementRef: ElementRef;
+
+  constructor(
+    private mapsAPILoader: MapsAPILoader,
+    private ngZone: NgZone
+  ) {}
+
+
+  ngOnInit() {
+    //set google maps defaults
+    this.zoom = 4;
+    this.latitude = 39.8282;
+    this.longitude = -98.5795;
+
+    //create search FormControl
+    this.searchControl = new FormControl();
+
+    //set current position
+    this.setCurrentPosition();
+
+    //load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+    
+    //this.setMarkers();
+    var uluru = {lat: 45.815399, lng: 15.966568};
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: uluru
+    });
+    
+    this.executeNearbySearch(uluru);
+    
+    //this.getMoveData();
+    google.maps.event.addListener(this.map,'dragend', () => 
+    {this.currentLocation = this.map.getCenter()
+     this.executeNearbySearch(this.currentLocation);});
+  }
+
+  getMoveData() {
+    this.currentLocation = this.map.getCenter();
+    console.log("Current location: " + this.currentLocation);
+    var newCurrLocation = this.currentLocation.toString();
+    newCurrLocation = newCurrLocation.replace('(', '');
+    newCurrLocation = newCurrLocation.replace(')', '');
+
+    var latlngArray = new Array();
+    latlngArray = newCurrLocation.split(",")
+    for (var a in latlngArray) {
+            latlngArray[a] = parseFloat(latlngArray[a]);
+    }
+    var newLat = latlngArray[0]
+    var newLng = latlngArray[1]
+    this.map.setCenter({
+        lat : newLat,
+        lng : newLng
+    });
+
+    this.executeNearbySearch(this.currentLocation);
+  }
+
+  private setCurrentPosition() {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 12;
+      });
+    }
+  }
+
+  setMarkers(){
+    var uluru = {lat: 45.815399, lng: 15.966568};
+    var uluru2 = {lat: 45.825313, lng: 15.976513};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 13,
+      center: uluru
+    });
+    this.marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      title: "Hello World!"
+    });
+    var marker2 = new google.maps.Marker({
+      position: uluru2,
+      map: map,
+      draggable: true,
+      animation: google.maps.Animation.DROP,
+      title: "Repsly Zgb"
+    });
+     
+    this.marker.addListener('click', this.toggleBounce);
+  }
+
+  toggleBounce() {
+  if (this.marker.getAnimation() !== null) {
+    this.marker.setAnimation(null);
+  } else {
+    this.marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+executeNearbySearch(currentLocation: any)
+{
+  // var uluru = {lat: 45.815399, lng: 15.966568};
+  // this.map = new google.maps.Map(document.getElementById('map'), {
+  //     zoom: 13,
+  //     center: currentLocation
+  //   });
+
+   this.request = {
+     location: currentLocation,
+     radius: '600',
+     type: ['bar']
+   };
+
+  var service = new google.maps.places.PlacesService(this.map);
+  
+  //service.nearbySearch(this.request, this.processResults);
+
+  service.nearbySearch(this.request, (results, status, pagination) => {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    this.createMarkers(results);
+    this.places = results;
+    console.log(this.places);
+
+    if (pagination.hasNextPage) {
+      var moreButton = <HTMLInputElement> document.getElementById('more');
+
+      moreButton.disabled  = false;
+
+      moreButton.addEventListener('click', function() {
+        moreButton.disabled = true;
+        pagination.nextPage();
+      });
+    }
+    }
+  });
+}
+
+// processResults(results, status, pagination) {
+//   if (status !== google.maps.places.PlacesServiceStatus.OK) {
+//     return;
+//   } else {
+//     console.log(results);
+//     this.createMarkers(results);
+//     this.places = results;
+
+//     if (pagination.hasNextPage) {
+//       var moreButton = <HTMLInputElement> document.getElementById('more');
+
+//       moreButton.disabled  = false;
+
+//       moreButton.addEventListener('click', function() {
+//         moreButton.disabled = true;
+//         pagination.nextPage();
+//       });
+//     }
+//   }
+// }
+
+createMarkers(places) {
+  var bounds = new google.maps.LatLngBounds();
+  var placesList = document.getElementById('places');
+
+  for (var i = 0, place; place = places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+
+    var marker = new google.maps.Marker({
+      map: this.map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+    //this.places = places;
+    //placesList.innerHTML += '<li style="border: 1px solid rgba(0,0,0,0.2); padding:5px;">' + place.name + '</li>';
+
+    bounds.extend(place.geometry.location);
+  }
+  this.map.fitBounds(bounds);
+  }
+
+  getUrlForPhoto(place: any)
+  {
+    var url = place.photos[0].getUrl({'maxWidth': 45, 'maxHeight': 45});
+    return url;
+  }
+
+}
